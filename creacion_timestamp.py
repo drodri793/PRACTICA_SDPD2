f9b510a02ae5c45d9095e949e2272350e40bc8fb
drodri793 <d.rodriguezmo.2023@alumnos.urjc.es>
2026-03-20 12:55:23 +0100

    Update creacion_timestamp.py

--- creacion_timestamp.py
+++ creacion_timestamp.py
@@ -7,5 +7,5 @@ df = pd.read_csv("tripadvisor_european_restaurants.csv")
 fecha_inicio = pd.Timestamp("2021-01-01 00:00:00")
 
 # Crear la nueva columna timestamp según el orden de las filas
-df["timestamp"] = fecha_inicio + pd.to_timedelta(range(len(df)), unit="m")
+df["timestamp"] = fecha_inicio + pd.to_timedelta(range(len(df)), unit="s")
 
